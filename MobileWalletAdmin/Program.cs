20b615b3107094f06c74488ed1278f7be0cd3302
AungPyae-Phyo <pyaea978@gmail.com>
2025-04-02 04:46:17 +0630

    Update Program.cs

--- MobileWalletAdmin/Program.cs
+++ MobileWalletAdmin/Program.cs
@@ -1,5 +1,9 @@
 using MudBlazor.Services;
 using MobileWalletAdmin.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+//using MobileWalletAdmin.Authentication;
+using MobileWalletAdmin.Services;
+using Blazored.LocalStorage;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +13,18 @@ builder.Services.AddMudServices();
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+//builder.Services.AddScoped<ApiService>();
+builder.Services.AddScoped<UserService>();
+builder.Services.AddBlazoredLocalStorage();
+
+
+//builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+
+builder.Services.AddScoped<HttpClient>(sp =>
+{
+    var client = new HttpClient { BaseAddress = new Uri("https://localhost:7249/") };
+    return client;
+});
 
 var app = builder.Build();
 
