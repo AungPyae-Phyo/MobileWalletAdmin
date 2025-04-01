d77fa1d7b776075930e5bf4416c70b27deb0b09d
AungPyae-Phyo <pyaea978@gmail.com>
2025-04-02 04:45:48 +0630

    Create Status.cs

---
+++ MobileWalletAdmin/Enum/Status.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MobileWalletAdmin.Enum
+{
+    public enum Status
+    {
+        [Display(Name = "New User")]
+        NEW_USER,
+
+        [Display(Name = "Closed")]
+        CLOSED,
+
+        [Display(Name = "Active")]
+        ACTIVE,
+
+        [Display(Name = "Pending")]
+        PENDING,
+
+        [Display(Name = "Inactive")]
+        INACTIVE,
+
+        [Display(Name = "On Hold")]
+        ON_HOLD,
+
+        [Display(Name = "Suspended")]
+        SUSPENDED,
+
+        [Display(Name = "Approved")]
+        APPROVE,
+
+        [Display(Name = "Rejected")]
+        REJECT,
+
+        [Display(Name = "Blocked")]
+        BLOCK,
+
+        [Display(Name = "Under Maintenance")]
+        UNDER_MAINTENANCE,
+
+        [Display(Name = "Locked")]
+        LOCKED,
+
+        [Display(Name = "Frozen")]
+        FROZEN,
+    }
+}
