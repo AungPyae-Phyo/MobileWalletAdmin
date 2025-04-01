2e2a2809f2e0b41a1b42a5ce74c13ff09cd557f5
AungPyae-Phyo <pyaea978@gmail.com>
2025-04-02 04:46:15 +0630

    Create UserModel.cs

---
+++ MobileWalletAdmin/Models/Users/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using MobileWalletAdmin.Enum;
+using System.Text.Json.Serialization;
+
+namespace MobileWalletAdmin.Models.Users
+{
+    public class UserModel
+    {
+        public string UserName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public string WalletID { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public Status Status { get; set; }
+        public decimal Balance { get; set; }
+        public DateTime createdOn { get; set; }
+    }
+}
