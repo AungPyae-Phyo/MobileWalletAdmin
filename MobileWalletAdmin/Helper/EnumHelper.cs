b03fbea27d2648b1684d4fa2e8aa7137ed96780c
AungPyae-Phyo <pyaea978@gmail.com>
2025-04-02 04:45:51 +0630

    Create EnumHelper.cs

---
+++ MobileWalletAdmin/Helper/EnumHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace MobileWalletAdmin.Helper
+{
+    public static class EnumHelper  
+    {
+        public static string GetEnumDescription(System.Enum value)
+        {
+            // Get the field info for the enum value
+            var field = value.GetType().GetField(value.ToString());
+
+            // Look for the Display attribute on the enum field
+            var attribute = field.GetCustomAttribute<DisplayAttribute>();
+
+            // If found, return the description, otherwise fallback to enum's string name
+            return attribute != null ? attribute.Name : value.ToString();
+        }
+    }
+}
