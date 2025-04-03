388c576c72f0fd402a0b743a484c4f794a6d1293
AungPyae-Phyo <pyaea978@gmail.com>
2025-04-03 16:45:35 +0630

    Create DocumentType.cs

---
+++ MobileWalletAdmin/Enum/DocumentType.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MobileWalletAdmin.Enum
+{
+    public enum DocumentType
+    {
+       
+        NRC,
+
+        [Display(Name = "Passport")]
+        PASSPORT,
+
+        [Display(Name = "Driver License")]
+        DRIVER_LICENSE,
+        UTILITY_BILL,
+        BANK_STATEMENT,
+        PAYSLIP,
+        TAX_DOCUMENT,
+        BIRTH_CERTIFICATE,
+        MARRIAGE_CERTIFICATE,
+        RESIDENCE_PERMIT,
+        WORK_PERMIT,
+        OTHER
+
+    }
+}
