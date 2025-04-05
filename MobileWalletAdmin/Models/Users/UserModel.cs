cc921dbc3d68b40a88c5e3ca689e545f29f3c45b
AungPyae-Phyo <pyaea978@gmail.com>
2025-04-06 04:32:23 +0630

    update: add UserID

--- MobileWalletAdmin/Models/Users/UserModel.cs
+++ MobileWalletAdmin/Models/Users/UserModel.cs
@@ -6,6 +6,7 @@ namespace MobileWalletAdmin.Models.Users
 {
     public class UserModel
     {
+        public string UserID { get; set; }
         public string UserName { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
