a4ccb94352219afe11a87b56f328b496a12453fc
AungPyae-Phyo <pyaea978@gmail.com>
2025-04-12 16:18:12 +0630

    Create WalletModel.cs

---
+++ MobileWalletAdmin/Models/Wallet/WalletModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MobileWalletAdmin.Models.Wallet
+{
+    public class WalletModel
+    {
+
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public decimal Balance { get; set; }
+        public string Name { get; set; }
+        public string AccountNumber { get; set; }
+        public DateTime LastModifiedOn { get; set; }
+        public string LastModifiedBy { get; set; }
+    }
+}
