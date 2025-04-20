29cfd718aeacd1efc1bd6613b8f5fbe87a4c2d5d
AungPyae-Phyo <pyaea978@gmail.com>
2025-04-20 22:30:27 +0630

    Create TransactionType.cs

---
+++ MobileWalletAdmin/Enum/TransactionType.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MobileWalletAdmin.Enum
+{
+    public enum TransactionType
+    {
+        [Display(Name = "Deposit")]
+        DEPOSIT,
+
+        [Display(Name = "Withdrawal")]
+        WITHDRAWAL,
+
+        [Display(Name = "Transfer")]
+        TRANSFER,
+
+        [Display(Name = "Loan")]
+        LOAN,
+
+        [Display(Name = "Payment")]
+        PAYMENT,
+
+        [Display(Name = "Refund")]
+        REFUND,
+
+        [Display(Name = "Exchange")]
+        EXCHANGE,
+
+        [Display(Name = "Investment")]
+        INVESTMENT,
+        DIVIDEND,
+        INTEREST,
+        FEE,
+        CHARGE,
+        REWARD,
+        BONUS,
+        COMMISSION,
+
+        [Display(Name = "Discount")]
+        DISCOUNT,
+
+        [Display(Name = "Gift Card")]
+        GIFT_CARD
+    }
+}
