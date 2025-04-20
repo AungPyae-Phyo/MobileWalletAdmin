b9f0252cbb707484335068f08a4aff41d80dbe08
AungPyae-Phyo <pyaea978@gmail.com>
2025-04-20 22:27:47 +0630

    Create LimitFeesModel.cs

---
+++ MobileWalletAdmin/Models/LimitFees/LimitFeesModel.cs
@@ -0,0 +1,17 @@
+﻿using MobileWalletAdmin.Enum;
+using System.Text.Json.Serialization;
+
+namespace MobileWalletAdmin.Models.LimitFees
+{
+    public class LimitFeesModel
+    {
+        public string? limit_fees_id { get; set; }
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public TransactionType? TransactionType { get; set; }
+        public string? description { get; set; }
+        public decimal min_amount { get; set; }
+        public decimal max_amount { get; set; }
+        public decimal percent_fees { get; set; }
+        public bool is_active { get; set; } = true;
+    }
+}
