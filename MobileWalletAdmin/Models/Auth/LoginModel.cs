c503ed2543ede649c7e01e2d91ccb37f3471ee5f
AungPyae-Phyo <pyaea978@gmail.com>
2025-05-07 03:01:43 +0630

    Create LoginModel.cs

---
+++ MobileWalletAdmin/Models/Auth/LoginModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MobileWalletAdmin.Models.Auth
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(4, ErrorMessage = "Password must be at least 4 characters.")]
+        public string Password { get; set; }
+    }
+
+
+    public class AuthResponse
+    {
+        public string Token { get; set; } = string.Empty;
+    }
+}
