1b5744f73e2546ed3b681594fbc7ef6f6364ae4e
AungPyae-Phyo <pyaea978@gmail.com>
2025-05-06 20:17:39 +0630

    Create TransactionHistoryModel.cs

---
+++ MobileWalletAdmin/Models/TransactionHistory/TransactionHistoryModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MobileWalletAdmin.Models.TransactionHistory
+{
+    public class TransactionHistoryModel
+    {
+        public string Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string SenderName { get; set; }
+        public string ReceiverName { get; set; }
+        public decimal Amount { get; set; }
+        public string TransactionType { get; set; }
+        public string? Description { get; set; }
+    }
+}
